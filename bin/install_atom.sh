d0b17ccce2a747ab3b53a153b80c28151df6e2c2
John Dahl <dunkar70@gmail.com>
2017-11-21 05:32:30 -0600

    minor updates

--- bin/install_atom.sh
+++ bin/install_atom.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-[[ $(which git) ]] || sudo apt-get install -y git
+[[ $(which git) ]] || sudo apt-get install -y git || exit 1
 
 local_file_location=/tmp/atom.deb
-remote_file_location=https://atom.io/download/deb
 
-
-wget -O /tmp/${local_file_location} ${remote_file_location} && \
+wget -O /tmp/${local_file_location} https://atom.io/download/deb && \
 sudo dpkg -i /tmp/${local_file_location} && \
 rm /tmp/${local_file_location}
