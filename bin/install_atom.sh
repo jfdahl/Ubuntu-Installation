71a59a97bead73323bdd7fa84f46f155591740e5
John Dahl <dunkar70@gmail.com>
2017-11-23 11:10:36 -0600

    Corrected local file locations

--- bin/install_atom.sh
+++ bin/install_atom.sh
@@ -3,7 +3,9 @@
 [[ $(which git) ]] || sudo apt-get install -y git || exit 1
 
 local_file_location=/tmp/atom.deb
+remote_file_location=https://atom.io/download/deb
 
-wget -O /tmp/${local_file_location} https://atom.io/download/deb && \
-sudo dpkg -i /tmp/${local_file_location} && \
-rm /tmp/${local_file_location}
+wget -O ${local_file_location} ${remote_file_location} && \
+sudo dpkg -i ${local_file_location} && \
+rm ${local_file_location}
+sudo apt-get install -f
