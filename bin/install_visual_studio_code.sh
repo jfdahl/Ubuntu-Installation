976738a67e94d438966067f50664487450b66462
John Dahl <dunkar70@gmail.com>
2018-01-01 12:54:45 -0600

    Added VSCode install script.

---
+++ bin/install_visual_studio_code.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+local_file_location=/tmp/visual_studio_code.deb
+remote_file_location=https://go.microsoft.com/fwlink/?LinkID=620884 # 64bit tarball
+remote_file_location=https://go.microsoft.com/fwlink/?LinkID=620882 # 64bit osx package
+remote_file_location=https://go.microsoft.com/fwlink/?LinkID=760868 # 64bit deb package
+
+wget -O ${local_file_location} ${remote_file_location} && \
+sudo dpkg -i ${local_file_location} && \
+rm ${local_file_location}
+sudo apt-get install -yf
+
+
+# install_location=/usr/share
+
+# wget -O ${local_file_location} "${remote_file_location}"
+# tar -xjf ${local_file_location} -C ${install_location}
+# ln -s ${install_location}/firefox/firefox /usr/bin
+# rm ${local_file_location}
