879d96fbf6bdb005dbb3d91588d72baa1ee9543b
John Dahl <dunkar70@gmail.com>
2018-01-01 09:13:40 -0600

    Fixed sudo

--- start.sh
+++ start.sh
@@ -3,18 +3,15 @@
 # See the README.md file for instructions.
 
 ip_addr=${1}
+user=root
 if [[ "${ip_addr}" = "local" ]]; then
     cp -r bin /tmp/
     cd /tmp/bin
     time bash sudo /tmp/bin/setup.sh
 else
     ssh_options='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
-    scp -r $ssh_options bin ${username}@${ip_addr}:/tmp/
-    time ssh $ssh_options ${username}@${ip_addr} bash sudo /tmp/bin/setup.sh
+    scp -r $ssh_options bin ${user}@${ip_addr}:/tmp/
+    time ssh $ssh_options ${user}@${ip_addr} sudo bash /tmp/bin/setup.sh
 fi
 
 exit 0
-
-ip_addr=10.0.0.226
-ssh_options='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
-ssh $ssh_options user@${ip_addr}
