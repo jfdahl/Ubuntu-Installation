fab0c2aee02d79cedfcf8de9dccd0b9422a48ca1
John Dahl <dunkar70@gmail.com>
2018-01-01 12:04:20 -0600

    Removed terminator from the package list.

--- bin/install_xfce.sh
+++ bin/install_xfce.sh
@@ -2,7 +2,7 @@
 
 [[ $(lspci | grep VirtualBox) ]] && VBOX=true || VBOX=false
 
-packages="xfce4 xfce4-whiskermenu-plugin gvfs alsa-utils xscreensaver mousepad terminator"
+packages="xfce4 xfce4-whiskermenu-plugin gvfs alsa-utils xscreensaver mousepad"
 $VBOX && packages="${packages} dkms build-essential"
 
 sudo apt install -y ${packages}
