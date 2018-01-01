36f2522bca381558f05a13b5bad2234a27913ae6
John Dahl <dunkar70@gmail.com>
2018-01-01 11:09:19 -0600

    Removed optional apps... they need to be installed manual as needed.

--- bin/install_xfce.sh
+++ bin/install_xfce.sh
@@ -6,8 +6,3 @@ packages="xfce4 xfce4-whiskermenu-plugin gvfs alsa-utils xscreensaver mousepad t
 $VBOX && packages="${packages} dkms build-essential"
 
 sudo apt install -y ${packages}
-
-sudo bash install_firefox.sh
-sudo bash install_atom.sh
-sudo bash install_python.sh
-sudo bash install_node.sh
