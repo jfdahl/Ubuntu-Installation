367ac4ffe1bbf9cc8e0481f175c65d6664385d53
John Dahl <dunkar70@gmail.com>
2018-01-01 09:29:28 -0600

    Changed install location to a variable.

--- bin/install_firefox.sh
+++ bin/install_firefox.sh
@@ -2,10 +2,11 @@
 
 local_file_location=/tmp/firefox.tar.bz2
 remote_file_location="https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
+install_location=/usr/share
 
 wget -O ${local_file_location} "${remote_file_location}"
-tar -xjf ${local_file_location} -C /usr/share/
-ln -s /usr/share/firefox/firefox /usr/bin
+tar -xjf ${local_file_location} -C ${install_location}
+ln -s ${install_location}/firefox/firefox /usr/bin
 rm ${local_file_location}
 
 # Setup desktop file and menu entries
@@ -14,8 +15,8 @@ cat >> /usr/share/applications/firefox.desktop << EOF
 Name=Firefox
 Comment=Web Browser.
 GenericName=Web Browser
-Exec=/usr/share/firefox/firefox %F
-Icon=/usr/share/firefox/browser/icons/mozicon128.png
+Exec=${install_location}/firefox/firefox %F
+Icon=${install_location}/firefox/browser/icons/mozicon128.png
 Type=Application
 StartupNotify=true
 Categories=GNOME;GTK;Internet;
