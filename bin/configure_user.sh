7d74df186f8f2da0f1d59647cf40e9a9294c82d2
John Dahl <dunkar70@gmail.com>
2018-01-01 21:48:38 -0600

    Cleanup.

--- bin/configure_user.sh
+++ bin/configure_user.sh
@@ -8,16 +8,12 @@ cat << EOF >> ${HOME}/.bashrc
 
 EOF
 
-
 #Configure Thunar
 xfconf-query --channel thunar --property /misc-full-path-in-title --create --type bool --set true
 xfconf-query --channel thunar --property /default-view --create --type string --set ThunarDetailsView
 xfconf-query --channel thunar --property /last-details-view-visible-columns --create --type string --set THUNAR_COLUMN_DATE_MODIFIED,THUNAR_COLUMN_GROUP,THUNAR_COLUMN_NAME,THUNAR_COLUMN_OWNER,THUNAR_COLUMN_PERMISSIONS,THUNAR_COLUMN_SIZE,THUNAR_COLUMN_TYPE
 xfconf-query --channel thunar --property /last-details-view-column-order --create --type string --set THUNAR_COLUMN_NAME,THUNAR_COLUMN_SIZE,THUNAR_COLUMN_TYPE,THUNAR_COLUMN_DATE_MODIFIED,THUNAR_COLUMN_OWNER,THUNAR_COLUMN_GROUP,THUNAR_COLUMN_PERMISSIONS,THUNAR_COLUMN_DATE_ACCESSED,THUNAR_COLUMN_MIME_TYPE
 xfconf-query --channel thunar --property /last-details-view-column-widths --create --type string --set 50,136,77,50,204,80,122,83,153
-
-#Configure xfce panel
-#xfconf-query --create --channel xfce4-panel --property /plugins/plugin-8/timezone --create --type string --set US/Central
 
 #Configure desktop
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/image-style --type int --set 0
@@ -41,7 +37,6 @@ dconf write /org/xfce/mousepad/preferences/view/indent-on-tab true
 dconf write /org/xfce/mousepad/preferences/view/right-margin-position 80
 dconf write /org/xfce/mousepad/preferences/view/tab-width 4
 dconf write /org/xfce/mousepad/preferences/view/auto-indent true
-
 dconf write /org/xfce/mousepad/preferences/window/path-in-title true
 dconf write /org/xfce/mousepad/preferences/window/statusbar-visible true
 
--- bin/install_xfce.sh
+++ bin/install_xfce.sh
@@ -2,7 +2,7 @@
 
 [[ $(lspci | grep VirtualBox) ]] && VBOX=true || VBOX=false
 
-packages="xfce4 xfce4-whiskermenu-plugin gvfs alsa-utils xscreensaver mousepad"
+packages="xfce4 xfce4-goodies gvfs alsa-utils xscreensaver mousepad" #xfce4-whiskermenu-plugin xfce4-statusnotifier-plugin 
 $VBOX && packages="${packages} dkms build-essential"
 
 sudo apt install -y ${packages}
