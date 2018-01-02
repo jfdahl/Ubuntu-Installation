cc7d63e848097a3e25a77e94fa39f4b99bf9b50b
John Dahl <dunkar70@gmail.com>
2018-01-02 05:54:27 -0600

    Moved install steps into functions and changed the default source file type from .deb to .tar.gz for more control.

--- bin/install_visual_studio_code.sh
+++ bin/install_visual_studio_code.sh
@@ -1,19 +1,38 @@
 #!/usr/bin/env bash
 
-local_file_location=/tmp/visual_studio_code.deb
-# remote_file_location=https://go.microsoft.com/fwlink/?LinkID=620884 # 64bit tarball
 # remote_file_location=https://go.microsoft.com/fwlink/?LinkID=620882 # 64bit osx package
-remote_file_location=https://go.microsoft.com/fwlink/?LinkID=760868 # 64bit deb package
 
-wget -O ${local_file_location} ${remote_file_location} && \
-sudo dpkg -i ${local_file_location} && \
-rm ${local_file_location}
-sudo apt-get install -yf
+function install_deb(){
+    local_file_location=/tmp/visual_studio_code.deb
+    remote_file_location=https://go.microsoft.com/fwlink/?LinkID=760868 # 64bit deb package
+    wget -O ${local_file_location} ${remote_file_location} && \
+    sudo dpkg -i ${local_file_location} && \
+    rm ${local_file_location}
+    sudo apt-get install -yf
+}
 
+function install_tarball(){
+    local_file_location=/tmp/visual_studio_code.tar.gz
+    remote_file_location=https://go.microsoft.com/fwlink/?LinkID=620884 # 64bit tarball
+    install_location=/usr/share
+    wget -O ${local_file_location} "${remote_file_location}"
+    tar -xzf ${local_file_location} -C ${install_location}
+    ln -s ${install_location}/VSCode-linux-x64/code /usr/bin
+    rm ${local_file_location}
 
-# install_location=/usr/share
+    cat >> /usr/share/applications/code.desktop << EOF
+    [Desktop Entry]
+    Name=Visual Studio Code
+    Comment=Text Editor.
+    GenericName=Text Editor
+    Exec=${install_location}/VSCode-linux-x64/code %F
+    Icon=${install_location}/VSCode-linux-x64/resources/app/resources/linux/code.png
+    Type=Application
+    StartupNotify=true
+    Categories=TextEditor;Development;
+    Terminal=false
+    MimeType=text/plain
+    EOF
+}
 
-# wget -O ${local_file_location} "${remote_file_location}"
-# tar -xjf ${local_file_location} -C ${install_location}
-# ln -s ${install_location}/firefox/firefox /usr/bin
-# rm ${local_file_location}
+install_tarball
