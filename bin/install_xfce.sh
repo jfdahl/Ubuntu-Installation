38f5bb7b3a80953171cf22b52e816bf88a6bf20e
John Dahl <dunkar70@gmail.com>
2018-01-06 03:37:36 -0600

    Moved the TODO reference to the user config script. Added the dconf-cli package for scripted user config.

--- bin/install_xfce.sh
+++ bin/install_xfce.sh
@@ -2,10 +2,7 @@
 
 [[ $(lspci | grep VirtualBox) ]] && VBOX=true || VBOX=false
 
-packages="xfce4 xfce4-goodies gvfs alsa-utils xscreensaver mousepad" #xfce4-whiskermenu-plugin xfce4-statusnotifier-plugin 
+packages="xfce4 xfce4-goodies gvfs alsa-utils xscreensaver mousepad dconf-cli" #xfce4-whiskermenu-plugin xfce4-statusnotifier-plugin
 $VBOX && packages="${packages} dkms build-essential"
 
 sudo apt install -y ${packages}
-
-# TODO:
-# Configure the panel
