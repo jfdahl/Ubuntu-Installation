d03af148e415bd4698fa81fc529627010d532be2
John Dahl <dunkar70@gmail.com>
2018-01-06 03:38:57 -0600

    Fixed some broken references created after an earlier refactoring.

--- bin/install_python.sh
+++ bin/install_python.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y python3-pip python3-setuptools && sudo pip3 install virtualenvwrapper
+[ -f /usr/local/bin/virtualenv ] || \
+sudo apt-get install -y python3-pip python3-setuptools && \
+sudo pip3 install virtualenvwrapper ||
+exit 1
 
-cat << EOF >> /home/public/bin/env.sh
+cat << EOF >> ~/bin/virtenv.sh
 # Python and VirtualEnv Settings:
 VIRTUALENVWRAPPER_PYTHON=\$(which python3)
 
@@ -12,9 +15,7 @@ WORKON_HOME=\${HOME}/VEs
 PROJECT_HOME=\${HOME}/Projects
 [ -d \$PROJECT_HOME ] || mkdir -p \$PROJECT_HOME
 
-script_loc=~${HOME}/.local/bin/virtualenvwrapper.sh
+script_loc=~/usr/local/bin/virtualenvwrapper.sh
 [ -f \${script_loc} ] && source \${script_loc}
-
-export PATH=${PATH}:${HOME}/.local/bin
 
 EOF
