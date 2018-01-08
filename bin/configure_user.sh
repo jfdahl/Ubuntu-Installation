f19417d52f5a341a8a41f1d88ca58680413d5ddd
John Dahl <dunkar70@gmail.com>
2018-01-08 08:25:55 -0600

    Added panel configuration, added a thunar preference, corrected the env.sh load location.

--- bin/configure_user.sh
+++ bin/configure_user.sh
@@ -2,7 +2,7 @@
 
 [ -d ~/bin ] || ln -s /user/public/bin ${HOME}/bin
 cd ${HOME}/bin
-cat << EOF >> ${HOME}/.bashrc
+cat << EOF >> ${HOME}/.profile
 
 [ -f ~/bin/env.sh ] || . ~/bin/env.sh
 
@@ -11,6 +11,7 @@ EOF
 #Configure Thunar
 xfconf-query --channel thunar --property /misc-full-path-in-title --create --type bool --set true
 xfconf-query --channel thunar --property /default-view --create --type string --set ThunarDetailsView
+xfconf-query --channel thunar --property /last-side-pane --create --type string --set ThunarTreePane
 xfconf-query --channel thunar --property /last-details-view-visible-columns --create --type string --set THUNAR_COLUMN_DATE_MODIFIED,THUNAR_COLUMN_GROUP,THUNAR_COLUMN_NAME,THUNAR_COLUMN_OWNER,THUNAR_COLUMN_PERMISSIONS,THUNAR_COLUMN_SIZE,THUNAR_COLUMN_TYPE
 xfconf-query --channel thunar --property /last-details-view-column-order --create --type string --set THUNAR_COLUMN_NAME,THUNAR_COLUMN_SIZE,THUNAR_COLUMN_TYPE,THUNAR_COLUMN_DATE_MODIFIED,THUNAR_COLUMN_OWNER,THUNAR_COLUMN_GROUP,THUNAR_COLUMN_PERMISSIONS,THUNAR_COLUMN_DATE_ACCESSED,THUNAR_COLUMN_MIME_TYPE
 xfconf-query --channel thunar --property /last-details-view-column-widths --create --type string --set 50,136,77,50,204,80,122,83,153
@@ -52,3 +53,71 @@ EOF
 
 # TODO:
 # Configure the panel
+cat << EOF > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+<?xml version="1.0" encoding="UTF-8"?>
+
+<channel name="xfce4-panel" version="1.0">
+  <property name="configver" type="int" value="2"/>
+  <property name="panels" type="array">
+    <value type="int" value="1"/>
+    <property name="panel-1" type="empty">
+      <property name="position" type="string" value="p=6;x=96;y=24"/>
+      <property name="size" type="uint" value="35"/>
+      <property name="length" type="uint" value="100"/>
+      <property name="position-locked" type="bool" value="true"/>
+      <property name="plugin-ids" type="array">
+        <value type="int" value="1"/>
+        <value type="int" value="2"/>
+        <value type="int" value="3"/>
+        <value type="int" value="4"/>
+        <value type="int" value="5"/>
+        <value type="int" value="6"/>
+        <value type="int" value="7"/>
+        <value type="int" value="8"/>
+        <value type="int" value="9"/>
+        <value type="int" value="10"/>
+        <value type="int" value="11"/>
+        <value type="int" value="12"/>
+        <value type="int" value="13"/>
+      </property>
+    </property>
+  </property>
+  <property name="plugins" type="empty">
+    <property name="plugin-1" type="string" value="whiskermenu"/>
+    <property name="plugin-2" type="string" value="directorymenu">
+      <property name="base-directory" type="string" value="/home/user"/>
+      <property name="file-pattern" type="string" value="*"/>
+      <property name="icon-name" type="string" value="user-home"/>
+    </property>
+    <property name="plugin-3" type="string" value="tasklist"/>
+    <property name="plugin-4" type="string" value="separator">
+      <property name="expand" type="bool" value="true"/>
+    </property>
+    <property name="plugin-5" type="string" value="systray"/>
+    <property name="plugin-6" type="string" value="pulseaudio">
+      <property name="enable-keyboard-shortcuts" type="bool" value="true"/>
+    </property>
+    <property name="plugin-7" type="string" value="netload"/>
+    <property name="plugin-8" type="string" value="clock">
+      <property name="timezone" type="string" value="US/Central"/>
+      <property name="digital-format" type="string" value=" | %r | "/>
+    </property>
+    <property name="plugin-9" type="string" value="actions">
+      <property name="items" type="array">
+        <value type="string" value="+lock-screen"/>
+        <value type="string" value="-switch-user"/>
+        <value type="string" value="+separator"/>
+        <value type="string" value="-suspend"/>
+        <value type="string" value="-hibernate"/>
+        <value type="string" value="-separator"/>
+        <value type="string" value="+shutdown"/>
+        <value type="string" value="+restart"/>
+        <value type="string" value="+separator"/>
+        <value type="string" value="+logout"/>
+        <value type="string" value="-logout-dialog"/>
+      </property>
+      <property name="ask-confirmation" type="bool" value="false"/>
+    </property>
+  </property>
+</channel>
+EOF
