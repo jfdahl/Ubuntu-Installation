ad255bd92485ffad7a33944bcae346a03f16e198
John Dahl <dunkar70@gmail.com>
2018-01-14 07:37:17 -0600

    Moved conda install location to ~/.local/share.

--- bin/configure_python.sh
+++ bin/configure_python.sh
@@ -23,11 +23,12 @@
 
 local_file_location=~/Miniconda3-latest-Linux-x86_64.sh
 remote_file_location="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+install_location=~/.local/share/miniconda3
 
 wget -O ${local_file_location} "${remote_file_location}"
-bash ${local_file_location} -b
+bash ${local_file_location} -b -p ${install_location}
 cat >> ~/bin/env.sh << EOF
 
-export PATH="/home/${USER}/miniconda3/bin:$PATH"
+export PATH="${install_location}/bin:$PATH"
 
 EOF
