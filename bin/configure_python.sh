7a905f0ebaf619d682124b7e3358ce34e3a69919
John Dahl <dunkar70@gmail.com>
2018-01-14 06:58:23 -0600

    Refactored to use Miniconda distribution.

--- bin/configure_python.sh
+++ bin/configure_python.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/env bash
 
-[ -f /usr/local/bin/virtualenv ] || \
-sudo apt-get install -y python3-pip python3-setuptools && \
-sudo pip3 install virtualenvwrapper ||
-exit 1
+# [ -f /usr/local/bin/virtualenv ] || \
+# sudo apt-get install -y python3-pip python3-setuptools && \
+# sudo pip3 install virtualenvwrapper ||
+# exit 1
+#
+# cat << EOF >> ~/bin/env.sh
+#
+# # Python and VirtualEnv Settings:
+# VIRTUALENVWRAPPER_PYTHON=\$(which python3)
+#
+# WORKON_HOME=\${HOME}/VEs
+# [ -d \$WORKON_HOME ] || mkdir -p \$WORKON_HOME
+#
+# PROJECT_HOME=\${HOME}/Projects
+# [ -d \$PROJECT_HOME ] || mkdir -p \$PROJECT_HOME
+#
+# script_loc=/usr/local/bin/virtualenvwrapper.sh
+# [ -f \${script_loc} ] && source \${script_loc}
+#
+# EOF
 
-cat << EOF >> ~/bin/env.sh
+local_file_location=~/Miniconda3-latest-Linux-x86_64.sh
+remote_file_location="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
 
-# Python and VirtualEnv Settings:
-VIRTUALENVWRAPPER_PYTHON=\$(which python3)
+wget -O ${local_file_location} "${remote_file_location}"
+bash ${local_file_location} -b
+cat >> ~/bin/env.sh << EOF
 
-WORKON_HOME=\${HOME}/VEs
-[ -d \$WORKON_HOME ] || mkdir -p \$WORKON_HOME
-
-PROJECT_HOME=\${HOME}/Projects
-[ -d \$PROJECT_HOME ] || mkdir -p \$PROJECT_HOME
-
-script_loc=/usr/local/bin/virtualenvwrapper.sh
-[ -f \${script_loc} ] && source \${script_loc}
+export PATH="/home/${USER}/miniconda3/bin:$PATH"
 
 EOF
