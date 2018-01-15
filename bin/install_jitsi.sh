df2a63b6a0a1091d3ba27a3d255e8f22e8986448
John Dahl <dunkar70@gmail.com>
2018-01-15 08:32:16 -0600

    Refactored test, added web server and an exit error code.

--- bin/install_jitsi.sh
+++ bin/install_jitsi.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # Pass the "-r" parameter to uninstall the Jitsi suite.
-if [["${1}" == "-r"]]; then
+if ["${1}" == "-r"]; then
   apt purge -y jigasi jitsi-meet jitsi-meet-web-config jitsi-meet-web jicofo jitsi-videobridge
   apt purge -y jigasi jitsi-meet jitsi-meet-web-config jitsi-meet-web jicofo jitsi-videobridge
 else
+  apt -y install nginx || exit 1
   echo 'deb https://download.jitsi.org stable/' >> /etc/apt/sources.list.d/jitsi-stable.list
   wget -qO -  https://download.jitsi.org/jitsi-key.gpg.key | apt-key add -
-  apt update && apt -y install jitsi-meet
-  echo "NOTE: To install a Let's Encrypt certificate, execute the following:"
-  echo "/usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh"
+  apt update && apt -y install jitsi-meet || exit 1
+  echo "\nNOTE: To install a Let's Encrypt certificate, execute the following:"
+  echo "\t/usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh"
 fi
