58ff2554de4c9fe72314ffbf0ff380d0f679cabe
John Dahl <dunkar70@gmail.com>
2018-01-15 19:31:30 -0600

    Fixed path reference.

--- bin/configure_python.sh
+++ bin/configure_python.sh
@@ -29,6 +29,6 @@ wget -O ${local_file_location} "${remote_file_location}"
 bash ${local_file_location} -b -p ${install_location}
 cat >> ~/bin/env.sh << EOF
 
-export PATH="${install_location}/bin:$PATH"
+export PATH="${install_location}/bin:\$PATH"
 
 EOF
