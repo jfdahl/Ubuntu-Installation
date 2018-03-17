82bec8fc97cbeed2ad40775fc9ec2702ad1480df
John Dahl <dunkar70@gmail.com>
2018-03-17 11:05:16 -0500

    Added a check for git

--- scripts/get_scripts.sh
+++ scripts/get_scripts.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -z $(which git) ]; then
+    echo "Please install git before running this script."
+    exit 1
+fi
 
 scripts_source="https://gitlab.com/jfdahl/myscripts.git"
 mkdir -p ~/Git
